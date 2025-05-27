cccb6d986021f864c530017deb95dc77936a2a65
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-05-27 21:24:06 +0100

    Popup working

--- apps/frontend/src/pages/image/ImagePopup.tsx
+++ apps/frontend/src/pages/image/ImagePopup.tsx
@@ -1,21 +1,25 @@
 import { Image } from '@wirralbears/backend-types';
 
 interface ImagePopupProps {
-  image: Image;
-  onClose: () => void;
+	image: Image;
+	onClose: () => void;
 }
 
 export default function ImagePopup({ image, onClose }: ImagePopupProps) {
-  const handleParentClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      console.log("Clicked outside the image, closing popup");
-      onClose();
-    }
-  }
+	const handleParentClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (e.target === e.currentTarget) {
+			console.log('Clicked outside the image, closing popup');
+			onClose();
+		}
+	};
 
-   return (
-    <div className='min-w-full min-h-full absolute bg-black top-0' onClick={handleParentClick}>
-
-    </div>
-   ) 
+	return (
+		<div
+			className="min-w-full min-h-full absolute top-0 left-0 flex items-center justify-center"
+			style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+			onClick={handleParentClick}
+		>
+			<img src={image.url} alt="" />
+		</div>
+	);
 }
