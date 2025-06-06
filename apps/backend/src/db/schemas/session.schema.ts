0dae888cb8aafc28332199bb40ca899a9e855482
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-06-06 11:51:48 +0100

    Updated session schemas

--- apps/backend/src/db/schemas/session.schema.ts
+++ apps/backend/src/db/schemas/session.schema.ts
@@ -2,10 +2,11 @@ import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { users } from './auth.schema';
 import { nanoid } from 'nanoid';
 import { SESSION_AGE_GROUPS, SESSION_ID_LENGTH } from '@wirralbears/constants';
+import { coaches } from './coach.schema';
 
 const WEEKDAY_LENGTH = 9;
 
-export const sessionDays = pgTable('coaches', {
+export const sessionDays = pgTable('session_days', {
 	id: varchar('id')
 		.primaryKey()
 		.$defaultFn(() => nanoid(SESSION_ID_LENGTH)),
@@ -14,7 +15,7 @@ export const sessionDays = pgTable('coaches', {
 	updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
-export const session = pgTable('coaches', {
+export const session = pgTable('sessions', {
 	id: varchar('id')
 		.primaryKey()
 		.$defaultFn(() => nanoid(SESSION_ID_LENGTH)),
@@ -23,7 +24,10 @@ export const session = pgTable('coaches', {
 		.references(() => sessionDays.id, { onDelete: 'cascade' }),
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at').defaultNow().notNull(),
-	age: integer('age').notNull(), 
+	age: integer('age').notNull(),
+	coach: varchar('coach')
+		.notNull()
+		.references(() => coaches.id, { onDelete: 'cascade' }),
 });
 
 export type SessionDay = typeof sessionDays.$inferInsert;
