36579e23e19ce27ec39e925d14a02284c759df27
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-06-10 23:32:26 +0100

    New type and games repo setup

--- apps/backend/src/db/schemas/games.schema.ts
+++ apps/backend/src/db/schemas/games.schema.ts
@@ -4,6 +4,16 @@ import { SESSION_ID_LENGTH } from '@wirralbears/constants';
 import { coaches } from './coach.schema';
 import { blogs } from './blog.schema';
 
+export const seasons = pgTable('seasons', {
+	id: varchar('id', { length: SESSION_ID_LENGTH })
+		.primaryKey()
+		.$defaultFn(() => nanoid(SESSION_ID_LENGTH)),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+	season: varchar('season', { length: 10 }),
+	gender: varchar('string', { length: 10 }).notNull(),
+});
+
 export const games = pgTable('games', {
 	id: varchar('id', { length: SESSION_ID_LENGTH })
 		.primaryKey()
@@ -15,6 +25,19 @@ export const games = pgTable('games', {
 	),
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+	gender: varchar('string', { length: 10 }).notNull(),
+	season: varchar('season', { length: SESSION_ID_LENGTH })
+		.notNull()
+		.references(() => seasons.id, { onDelete: 'cascade' }),
 });
 
 export type game = typeof games.$inferSelect;
+export type season = typeof seasons.$inferSelect;
+export type gameInsert = typeof games.$inferInsert;
+export type seasonInsert = typeof seasons.$inferInsert;
+
+export type GamesBySeason = {
+	season: string;
+	seasonId: string;
+	games: game[];
+}[];
