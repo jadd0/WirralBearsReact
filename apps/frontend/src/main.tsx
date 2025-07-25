1dce9599b9488396cf0c927cd745a70e462c4625
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-07-25 11:42:49 +0100

    Implemented vercel analytics

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,12 +5,14 @@ import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@lib/query';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
 				<App />
+				<Analytics />
 			</BrowserRouter>
 		</QueryClientProvider>
 	</StrictMode>
