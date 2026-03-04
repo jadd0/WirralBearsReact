2195704163e3f074893b34ed9f15fe8c39f9ac59
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2026-03-04 00:59:41 +0000

    working banner

--- lib/frontend/components/layout/LogoBanner.tsx
+++ lib/frontend/components/layout/LogoBanner.tsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 
 export function LogoBanner({ className = "" }) {
   return (
-    <div className="w-screen flex justify-center mb-10">
-      {/* <Link href="/" className=" my-4 max-w-150">
-        <Image
-          src={"/images/WirralBearsBanner.png"}
-          className={`bear ${className}`}
+    <div className="w-full flex justify-center mb-10">
+      <Link href="/" className=" my-4 max-w-150">
+        <img
+          src="/images/WirralBearsBanner.png"
           alt="Wirral Bears Logo"
-          fill
+          className={className}
           style={{ objectFit: "cover" }}
         />
-      </Link> */}
+      </Link>
     </div>
   );
 }
